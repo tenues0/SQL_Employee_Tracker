8870d1a5df663c548bc277c5a9d3d61ff64a03c0
Theodore Ayoub <theodore.ayoub@gmail.com>
2022-06-16 17:49:43 -0700

    SQL schema coded

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// importing dotenv
+require('dotenv').config()
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3001
+  }
+);
+
+module.exports = sequelize;
